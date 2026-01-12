c563debe3bf149b82b22d3860f47b961ce89a3b0
Célio Roberto <celio.roberto@tcmgo.tc.br>
2026-01-12 19:27:10 -0300

    feat: completed static ui

--- README.md
+++ README.md
@@ -1,36 +1,19 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+Projeto lista de tarefas
+1 - criar projeto em next.js
+npx create-next-app@latest .
+ 
+2 - criar repositório no git hub para o projeto
+git init
+git add
+git commit
+git branch -m main
+git remote add origin 'colocar o link do projeto no github'
+git push origin main
+ 
+3 - instalar biblioteca de componentes shadcn https://ui.shadcn.com
+npx shadcn@latest init
 
-## Getting Started
+3.1 - instalar componente do shadcn no projeto
+Vá até installation, copie o código e cole no terminal. Exemplo o componente button: npx shadcn@latest add button
 
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+3.2 - o shadcn utiliza os ícones da Lucide https://lucide.dev
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,175 @@
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogCancel,
+  AlertDialogAction,
+  AlertDialogHeader,
+  AlertDialogFooter,
+} from "@/components/ui/alert-dialog"
+import {
+  Plus,
+  List,
+  CircleCheck,
+  CheckCircle2,
+  Clock,
+  SquarePen,
+  Trash,
+  Square,
+  ListCheck,
+  Sigma,
+} from "lucide-react"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 const home = () => {
   return (
-    <div>ola célio</div>
+    <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <Card className="w-lg">
+        <CardHeader className="flex gap-2">
+          <Input placeholder="Adicionar tarefa" />
+          <Button className="cursor-pointer">
+            <Plus />
+            Cadastrar
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <Separator className="mb-4" />
+          <div className="flex gap-2 mb-4">
+            <Badge className="cursor-pointer" variant="default">
+              <List />
+              Todas
+            </Badge>
+            <Badge className="cursor-pointer" variant="outline">
+              <Clock />
+              Em andamento
+            </Badge>
+            <Badge className="cursor-pointer" variant="outline">
+              <CheckCircle2 />
+              Concluídas
+            </Badge>
+          </div>
+
+          <div className="mt-4 border-t border-b divide-y">
+            <div className="h-14 flex justify-between items-center">
+              <div className="w-1 h-full bg-green-300"></div>
+              <p className="flex-1 px-2 text-sm">Estudar React</p>
+              <div className="flex gap-2 items-center">
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <SquarePen size={16} className="cursor-pointer" />
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Editar tarefa</DialogTitle>
+                    </DialogHeader>
+                    <div className="flex gap-2">
+                      <Input />
+                      <Button className="cursor-pointer">Gravar</Button>
+                    </div>
+                  </DialogContent>
+                </Dialog>
+                <Trash size={16} className="cursor-pointer" />
+              </div>
+            </div>
+
+            <div className="h-14 flex justify-between items-center">
+              <div className="w-1 h-full bg-green-300"></div>
+              <p className="flex-1 px-2 text-sm">Estudar Bola</p>
+              <div className="flex gap-2 items-center">
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <SquarePen size={16} className="cursor-pointer" />
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Editar tarefa</DialogTitle>
+                    </DialogHeader>
+                    <div className="flex gap-2">
+                      <Input />
+                      <Button className="cursor-pointer">Gravar</Button>
+                    </div>
+                  </DialogContent>
+                </Dialog>
+                <Trash size={16} className="cursor-pointer" />
+              </div>
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className="flex flex-col gap-3 mt-4 w-full">
+            {/* Linha resumo */}
+            <div className="flex justify-between items-center w-full">
+              <div className="flex gap-2 items-center">
+                <ListCheck size={18} />
+                <p className="text-xs">Tarefas concluídas (3 / 3)</p>
+              </div>
+
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button
+                    className="text-xs h-7 cursor-pointer"
+                    variant="outline"
+                  >
+                    <Trash />
+                    Limpar tarefas concluídas
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>
+                      Tem certeza que deseja excluir "X" tarefas concluídas?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                      Esta ação não pode ser desfeita. Isso excluirá
+                      permanentemente as tarefas concluídas.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                    <AlertDialogAction>Continuar</AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </div>
+
+            {/* Barra de progresso */}
+            <div className="h-2 w-full bg-gray-100 rounded-md">
+              <div
+                className="h-full bg-blue-500 rounded-md"
+                style={{ width: "80%" }}
+              ></div>
+            </div>
+            {/* Total da barra de progresso */}
+            <div className="flex justify-end items-center gap-2">
+              <Sigma size={18} />
+              <p className="text-xs">3 tarefas concluídas</p>
+            </div>
+          </div>
+        </CardFooter>
+      </Card>
+    </main>
   )
 }
 
